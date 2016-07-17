9f060956cdd50c2cc1b500381bbef071b22661ea
Sergey <virtues@ukr.net>
2016-07-17 21:24:41 +0300

    log4j is added

---
+++ src/main/java/com/lisovskyi/app/listeners/LoggingWebDriverEventListener.java
@@ -0,0 +1,146 @@
+package com.lisovskyi.app.listeners;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+import org.testng.Reporter;
+
+
+public class LoggingWebDriverEventListener implements WebDriverEventListener {
+    private static final Logger logger = LogManager.getLogger(LoggingWebDriverEventListener.class); 
+   
+    
+    private By lastFindBy;
+    private String originalValue;   
+ 
+    public void afterChangeValueOf(WebElement element, WebDriver driver) {
+    	String message = String.format(
+    			"WebDriver changed value in element found by '%s' from '%s' to '%s'",
+        		getElementLocator(element) /*lastFindBy*/, originalValue, element.getAttribute("value"))
+        		.toString();
+    	// Log the message to the logger
+        logger.info(message);
+        // Log the message to the reporter
+        Reporter.log(message);
+    }
+
+    public void afterClickOn(WebElement element, WebDriver driver) {
+    	String message = String.format("WebDriver clicked on element found by '%s'", getElementLocator(element));
+        logger.info(message);
+        Reporter.log(message); 
+    }
+
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+    	String message = String.format("WebDriver found element '%s'", by);
+        logger.info(message);
+        Reporter.log(message);
+    }
+
+    public void afterNavigateBack(WebDriver driver) {
+    	logger.debug("WebDriver navigated back");
+    }
+
+    public void afterNavigateForward(WebDriver driver) {
+    	logger.debug("WebDriver navigated forward");
+    }
+
+    public void afterNavigateTo(String url, WebDriver driver) {
+    	String message = String.format("WebDriver navigated to '%s'", url);
+        logger.info(message);
+        Reporter.log(message);
+    }
+
+    public void afterScript(String script, WebDriver driver) {
+    	logger.debug("WebDriver run script '{}'", script);
+    }
+
+    public void beforeChangeValueOf(WebElement element, WebDriver driver) {
+        originalValue = element.getAttribute("value");
+        logger.trace("WebDriver changing value in element found by '{}' from '{}'",
+        		getElementLocator(element) /*lastFindBy*/, originalValue);        
+    }
+
+    public void beforeClickOn(WebElement element, WebDriver driver) {
+    	logger.trace("WebDriver clicking on element found by '{}'", getElementLocator(element));
+    }
+
+    public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+        lastFindBy = by;
+        logger.trace("WebDriver finding element '{}'", by);
+    }
+
+    public void beforeNavigateBack(WebDriver driver) {
+    	logger.trace("WebDriver navigating back from '{}'", driver.getCurrentUrl());
+    }
+
+    public void beforeNavigateForward(WebDriver driver) {
+    	logger.trace("WebDriver navigating forward");
+    }
+
+    public void beforeNavigateTo(String url, WebDriver driver) {
+        logger.trace("WebDriver navigating to '{}'", url);
+    }
+
+    public void beforeScript(String script, WebDriver driver) {
+    	logger.trace("WebDriver running script '{}'", script);
+    }
+
+    public void onException(Throwable error, WebDriver driver) {
+        if (error.getClass().equals(NoSuchElementException.class)){
+            logger.error("WebDriver error: Element not found '{}'", lastFindBy);
+        } else {
+            logger.error("WebDriver error: ", error);
+        }       
+    }
+    
+    private String getElementLocator(WebElement element) {
+        return element.toString().substring(element.toString().indexOf(">") + 2, element.toString().lastIndexOf("]"));
+    }
+    
+    private String getElementVisibleText(WebElement element) {
+    	String visibleText = "unknown";
+        String text = element.getText();
+        String title = element.getAttribute("title");
+        String alt = element.getAttribute("alt");
+        
+		if (text != null && !text.isEmpty()) {
+			visibleText = text;
+		} else if (title != null && !title.isEmpty()) {
+			visibleText = title;
+		} else if (alt != null && !alt.isEmpty()) {
+			visibleText = alt;
+		}
+		
+		return visibleText;
+    }
+    
+    private String getElementIdentity(WebElement element) {
+    	String elementName = "unknown";
+        String id = element.getAttribute("id");
+        String name = element.getAttribute("name");
+        
+        if (id != null && !id.isEmpty()) {
+        	elementName =  "id: " + id;
+        } else if (name != null && !name.isEmpty()) {
+        	elementName =  "name: " + name;
+        }
+        
+        return elementName;
+    }
+
+	public void afterNavigateRefresh(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void beforeNavigateRefresh(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}    
+ 
+}
